d4afc61d08f351d9126a7c7804450f39ff524815
MustafaSamanci <samancimustafa@gmail.com>
2022-11-18 10:14:25 +0100

    Add files via upload

---
+++ FizzBuzzerTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzerTest
+    {
+        [TestCase(1,1)]
+        [TestCase(2, 2)]
+        [TestCase(4, 4)]
+      
+       
+
+
+        public void GetOutput_WhenCalled(int num, int res)
+        {
+            // a simple example to start you off
+            Assert.AreEqual(num, FizzBuzz1(res));
+        }
+
+
+        public static int FizzBuzz1(int res)
+        {
+            return res;
+
+        }
+}
+}
