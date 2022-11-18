de2803b2e54e2c0aa8bd27ce70e634c79f227d32
MustafaSamanci <samancimustafa@gmail.com>
2022-11-18 10:41:53 +0100

    Update FizzBuzzerTest.cs

--- FizzBuzzerTest.cs
+++ FizzBuzzerTest.cs
@@ -12,7 +12,7 @@ namespace FizzBuzz
     public class FizzBuzzerTest
     {
 
-        [TestCase(1, 1)]
+   
         [TestCase(3, "Fizz")]
         [TestCase(6, "Fizz")]
         [TestCase(123,"Fizz")]
